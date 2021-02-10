c204de2b5d2c9c1f7675084d785f78f2d0eaa2f0
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 19:15:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ function viewCart() {
   // write your code here
   var list = [];
   if (!cart.length) {
-    return 'Your shopping cart is empty';
+    return 'Your shopping cart is empty.';
   } else if(cart.length === 1) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].price}.`
   } else {
     for (let i = 1; i < cart.length; i++) {
       if (i === cart.length - 1){
-        list.push(` ${cart[i].itemName} at $${cart[i].price}.`)
+        list.push(` ${cart[i].itemName} at $${cart[i].price}`)
       }
       list.push(` ${cart[i].itemName} at $${cart[i].price}, and`);
     }
