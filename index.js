2f8bc4effe1d63ad319a50960e1eb95afd28adb2
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 20:27:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
     if (item === cart[i].itemName) {
-      delete cart[i];
+      cart.splice(i, 1);
       return cart;
     }
       return 'That item is not in your cart.'
