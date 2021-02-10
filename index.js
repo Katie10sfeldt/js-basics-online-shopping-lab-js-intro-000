5e33084bdf5f34b1d443cbd6ab5527a67d118476
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 18:49:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function addToCart(item) {
  // write your code here
  var price = Math.floor(Math.random() * 10) + 1;
  var product = {};
- product = {itemName: `${item}`, price: `${price}`}
+ product = {'itemName': `${item}`, 'price': `${price}`}
  return `${product.itemName} has been added to your cart.`
 }
 
