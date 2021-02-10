b9b6989aa8e7dc1c95979ac6c49767bb878c8366
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 20:27:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,9 +52,9 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     }
-      return 'That item is not in your cart.'
-
   }
+  return 'That item is not in your cart.'
+
 }
 
 function placeOrder(cardNumber) {
