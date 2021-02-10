414f6ba4a55c0024d2267cdbe1117b4ef4c57428
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 20:21:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,20 +22,17 @@ function addToCart(item) {
 function viewCart() {
   if(cart.length === 0){
     return `Your shopping cart is empty.`
-
   } else if(cart.length ===1){
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
-
   } else {
-
     var start = `In your cart, you have`
     var mid = []
     var end = `, and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
 
     for (var i = 0; i < cart.length-1; i++){
     mid.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
-            }
-         }
+    }
+  }
   return start + mid + end
 }
 
